4cb1e1e2edbcb50fbbec0c531844700ba63c6533
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-04-08 20:02:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-function changecolor(activecolor) {
-	let square=document.querySelectorAll('.square');
-
-	for(let i=0;i<square.length;i++)
-		{
-			if(i+1!==activecolor)
-			{
-				squares[i].style.backgroundColor = '#6F4E37';
-			}
-			else
-			{
-				squares[i].style.backgroundColor = '#E6E6FA';
-			}
-		}
+function changecolor(activeSquare) {
+    var squares = document.querySelectorAll('.square');
+    for (var i = 0; i < squares.length; i++) {
+        if (i + 1 !== activeSquare) {
+            squares[i].style.backgroundColor = '#6F4E37'; // Coffee
+        } else {
+            squares[i].style.backgroundColor = '#E6E6FA'; // Lavender
+        }
+    }
 }
