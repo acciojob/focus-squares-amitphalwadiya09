3b84133b65e619db27f628eb5f58bc964964843a
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-04-08 20:18:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-function changecolor(activeSquare) {
-    var squares = document.querySelectorAll('.square');
-    for (var i = 0; i < squares.length; i++) {
-        if (i + 1 !== activeSquare) {
-            squares[i].style.backgroundColor = 'rgb(111, 78, 55)'; // Coffee
-        } else {
-            squares[i].style.backgroundColor = 'rgb(230, 230, 250)'; // Lavender
+var prevColor = null;
+
+        function changeColor(activeSquare) {
+            var squares = document.querySelectorAll('.square');
+            for (var i = 0; i < squares.length; i++) {
+                if (i + 1 !== activeSquare) {
+                    prevColor = squares[i].style.backgroundColor;
+                    squares[i].style.backgroundColor = 'rgb(111, 78, 55)'; // Coffee
+                } else {
+                    squares[i].style.backgroundColor = 'rgb(230, 230, 250)'; // Lavender
+                }
+            }
         }
-    }
-}
-function resetColor() {
+
+        function resetColor() {
             var squares = document.querySelectorAll('.square');
             for (var i = 0; i < squares.length; i++) {
                 squares[i].style.backgroundColor = prevColor ? prevColor : 'rgb(230, 230, 250)'; // Revert to previous color or Lavender
