fd0497ca90494ba7952cd9f702126188951cdceb
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-04-08 20:06:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function changecolor(activeSquare) {
     var squares = document.querySelectorAll('.square');
     for (var i = 0; i < squares.length; i++) {
         if (i + 1 !== activeSquare) {
-            squares[i].style.backgroundColor = 'rgb(230, 230, 250)'; // Coffee
+            squares[i].style.backgroundColor = 'rgb(111, 78, 55)'; // Coffee
         } else {
-            squares[i].style.backgroundColor = 'rgb(111, 78, 55)'; // Lavender
+            squares[i].style.backgroundColor = 'rgb(230, 230, 250)'; // Lavender
         }
     }
 }
