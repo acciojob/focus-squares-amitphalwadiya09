7889bde5307e58b55003c670d17a8c3595dd0fb3
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-04-08 20:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function changecolor(activecolor) {
+	let square=document.querySelectorAll('.square');
+
+	for(let i=0;i<square.length;i++)
+		{
+			if(i+1!==activecolor)
+			{
+				squares[i].style.backgroundColor = '#6F4E37';
+			}
+			else
+			{
+				squares[i].style.backgroundColor = '#E6E6FA';
+			}
+		}
+}
